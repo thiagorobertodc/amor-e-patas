5b83974234dd2f4097208fe36bc2406f83229cea
Thiago Roberto <thiago@nursa.com>
2024-12-21 18:39:58 -0300

    feat: add svg icon to nav bar

--- src/components/BasicLayout/Navbar/Navbar.tsx
+++ src/components/BasicLayout/Navbar/Navbar.tsx
@@ -7,15 +7,21 @@ export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
-        // Try multiple approaches
         className="backgroundColor"
         classes={{ root: "backgroundColor" }}
         position="static"
       >
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Amor e Patas
-          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+            <img
+              src="src/assets/images/aep.svg"
+              alt="Logo"
+              style={{ width: 24, height: 24 }}
+            />
+            <Typography variant="h6" component="div">
+              Amor e Patas
+            </Typography>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
